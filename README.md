532c2d1e67d24ab2781de94c800447d579fad710
Ryan M. Waite <ryanwaite28@gmail.com>
2018-06-18 10:50:30 -0400

    initial

---
+++ README.md
@@ -0,0 +1,40 @@
+# Udacity Mentorship
+### My Code Reviews
+
+![Udacity Logo](img/udacity-2.png)
+
+https://www.udacity.com/start-mentoring
+> Get paid to mentor your peers and expand your knowledge. <br/>
+> Make a difference by becoming a Udacity mentor
+
+Here is a website that lists all of my Udacity project/code reviews!
+
+Data is exported as csv file.
+Using this to convert to objects: https://github.com/evanplaice/jquery-csv
+
+Here is an example object, converted from csv:
+
+```javascript
+{
+  assigned_at: "2018-03-06T17:29:21Z",
+  completed_at: "2018-03-06T19:40:41Z",
+  id: "111111",
+  price (USD): "15.0",
+  project_id: "302",
+  project_name: "MyReads: A Book Lending App",
+  result: "passed",
+  status: "completed"
+}
+```
+
+Using:
+* Materialize CSS
+* jQuery
+* jQuery CSV
+* AngularJS
+* Fetch API
+
+---
+
+# Ryan M. Waite
+### Web & Mobile Developer
---
+++ js/script.js
@@ -0,0 +1,67 @@
+const App = angular.module('App', []);
+
+App.controller('AppCtrl', ['$scope', function($scope){
+
+  $(document).ready(function(){
+    // $('select').formSelect();
+  });
+
+  //
+
+  $scope.reviewsList = {
+    "May 2018": { name: "May 2018", href: "reviews/may_2018.csv" }
+  };
+
+  $scope.currentReview = {};
+
+  // Functions
+
+  function fetch_review_csv(href) {
+    return fetch(href).then(function(r){ return r.text() })
+  }
+
+  function add_reviews_option(review) {
+    return new Promise(function(resolve, reject){
+      fetch_review_csv(review.href)
+      .then(function(csvText){
+        $scope.reviewsList[review.name].csvText = csvText;
+        $scope.reviewsList[review.name].reviews = $.csv.toObjects(csvText);;
+        $('#review-select').append('<option value="' + review.name + '">' + review.name + '</option>');
+        return resolve();
+      })
+    });
+  }
+
+  function add_all_reviews_options() {
+    return new Promise(function(resolve, reject){
+      let promises = [];
+      Object.keys($scope.reviewsList).forEach(function(key){
+        let review = $scope.reviewsList[key];
+        promises.push(add_reviews_option(review));
+      });
+      Promise.all(promises)
+      .then(function(values){
+        return resolve();
+      })
+    });
+  }
+
+  function init_select_listener() {
+    $('#review-select').change(function(){
+      let opt = $(this).val();
+      if($scope.reviewsList[opt]) {
+        $scope.currentReview = $scope.reviewsList[opt];
+        $scope.$apply();
+      }
+    });
+  }
+
+  add_all_reviews_options()
+  .then(function(){
+    console.log('done loading all reviews');
+    console.log( $scope);
+    $('select').formSelect();
+    init_select_listener();
+  })
+
+}]);
